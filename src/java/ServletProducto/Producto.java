2f61398c8ec095f4289ca6241b9f7628dc074518
AleGB <30881942+AleGB@users.noreply.github.com>
2017-10-12 08:35:18 -0500

    Update Producto.java

--- src/java/ServletProducto/Producto.java
+++ src/java/ServletProducto/Producto.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ServletProducto;
 
 import java.io.IOException;
@@ -15,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  *
- * @author Alumno
+ * @author Karla
  */
 @WebServlet(name = "Serie", urlPatterns = {"/Serie"})
 public class Producto extends HttpServlet {
